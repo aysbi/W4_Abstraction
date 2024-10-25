17f6da627ab0d3e057e53f8d1b92680cd9a50e84
Aysel Bilmez <aybilmez@gmail.com>
2024-10-25 10:06:13 +0300

    Proje dosyası ekle.

---
+++ W4_Abstraction/Program.cs
@@ -0,0 +1,12 @@
+﻿using W4_Abstraction;
+
+
+    // Sirket nesneleri oluşturalım
+    Sirket yazilimci = new YazilimGelistirici("Ahmet", "Yılmaz", "Yazılım Gelistirici");
+    Sirket projeYoneticisi = new ProjeYoneticisi("Hasan", "Çıldırmış", "Proje Yönetimi");
+    Sirket satisTemsilcisi = new SatisTemsilcisi("Merve", "Kara", "Satış Temsilcisi");
+
+    // Görevlerini çağıralım
+    yazilimci.Gorev();
+    projeYoneticisi.Gorev();
+    satisTemsilcisi.Gorev();
---
+++ W4_Abstraction/Sirket.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W4_Abstraction
+{
+    public abstract class Sirket  //abstract sinifi olusturuyoruz 
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Departman { get; set; }
+
+        public Sirket(string ad, string soyad, string departman)  //constructor olusturuyoruz 
+        {
+            Ad = ad;
+            Soyad = soyad;
+            Departman = departman;
+        }
+
+        public abstract void Gorev(); //abstract metodu olusturuyoruz
+    }
+
+    public class YazilimGelistirici : Sirket //inheritance ornegi 
+    {
+        public YazilimGelistirici(string ad, string soyad, string departman) 
+            : base(ad, soyad, departman)  //abstract sinif oldugu icin constructor tanimlamamiz lazim
+        { }
+        public override void Gorev() //override ornegi 
+        {
+            Console.WriteLine($"{Ad} {Soyad} : {Departman} olarak calisiyorum");
+        }
+
+    }
+
+    public class ProjeYoneticisi : Sirket
+    {
+        public ProjeYoneticisi(string ad, string soyad, string departman)
+            : base(ad, soyad, departman)
+        { }
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} : {Departman} olarak calisiyorum");
+        }
+    }
+
+    public class SatisTemsilcisi : Sirket
+    {
+        public SatisTemsilcisi(string ad, string soyad, string departman)
+            : base(ad, soyad, departman)
+        { }
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} : {Departman} olarak calisiyorum");
+        }
+    }
+}
